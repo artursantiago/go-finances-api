f185f497233fd84ea17aa95441ec0c1a878fe1b2
Artur Santiago <artursantiago@info.ufrn.br>
2021-01-26 16:53:58 -0300

    getting transactions and balance

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -10,8 +10,12 @@ const transactionsRepository = new TransactionsRepository();
 transactionRouter.get('/', (request, response) => {
   try {
     const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
 
-    return response.json(transactions);
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -21,12 +25,14 @@ transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
 
-    const createTransaction = new CreateTransactionService(transactionsRepository);
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
 
     const transaction = createTransaction.execute({
       title,
       value,
-      type
+      type,
     });
 
     return response.json(transaction);
